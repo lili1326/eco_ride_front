06c177f4af62714b5660ab2e2a17c572ace4382f
aurelie <lieange1326@outlook.com>
2025-05-22 13:31:04 +0200

    Update CORS config for Netlify

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
  // config.js
 export const API_URL = window.location.hostname === "localhost"
   ? "http://localhost:8000"
-  : "https://eco-ride-back.herokuapp.com";
+: "https://eco-ride-back-d7c8f5e4a3e6.herokuapp.com"; // API en production
