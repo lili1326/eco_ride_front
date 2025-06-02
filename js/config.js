f006047da81e278f12c3cb3613064ef7f90b11f0
aurelie <lieange1326@outlook.com>
2025-06-02 15:57:54 +0200

    feat: resposive et .env

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
  // config.js
+
+
  const isLocalhost =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
@@ -6,3 +8,6 @@
 export const API_URL = isLocalhost
   ? "http://localhost:8000"
   : "https://eco-ride-back-d7c8f5e4a3e6.herokuapp.com"; 
+  console.log("API_URL utilisée :", API_URL);
+console.log("hostname détecté :", window.location.hostname);
+console.log("Origin vue par le navigateur :", window.origin);
